3fdf199e8ae4d5400f02864a39aa87bed2711496
MrWollu <michaeldw217@hotmail.com>
2018-03-05 14:30:04 -0600

    Login
    Finished the main code for creating the login screen. I still trying to
    figure out how to connect the server and the client.

--- app/src/main/java/edu/uark/uarkregisterapp/LandingActivity.java
+++ app/src/main/java/edu/uark/uarkregisterapp/LandingActivity.java
@@ -3,11 +3,22 @@ package edu.uark.uarkregisterapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 import android.view.View;
 
 import edu.uark.uarkregisterapp.models.transition.ProductTransition;
 
+
 public class LandingActivity extends AppCompatActivity {
+
+	private static EditText username;
+	private static EditText password;
+	private static Button login_button;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -15,11 +26,30 @@ public class LandingActivity extends AppCompatActivity {
 		setContentView(R.layout.activity_landing);
 	}
 
-	public void displayAllProductsButtonOnClick(View view) {
+	/*public void displayAllProductsButtonOnClick(View view) {
 		this.startActivity(new Intent(getApplicationContext(), ProductsListingActivity.class));
-	}
+	}*/
 
-	public void createProductButtonOnClick(View view) {
+	public void createLoginButtonOnClick(View view) {
+		username = (EditText)findViewById(R.id.Employee_ID);
+		password = (EditText)findViewById(R.id.Password);
+		login_button = (Button)findViewById(R.id.button_create_Employee);
+
+		login_button.setOnClickListener(
+				new View.OnClickListener() {
+					@Override
+					public void onClick(View v) {
+						if(username.getText().toString().equals("user") &
+									password.getText().toString().equals("pass")){
+								Toast.makeText(LandingActivity.this,"Employee Exists", Toast.LENGTH_SHORT).show();
+						}
+						else{
+							Toast.makeText(LandingActivity.this,"No Employee Exists \n Please create your firsst Employee", Toast.LENGTH_SHORT).show();
+						}
+					}
+				}
+        );
+
 		Intent intent = new Intent(getApplicationContext(), ProductViewActivity.class);
 
 		intent.putExtra(
